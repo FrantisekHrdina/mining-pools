4581f82a330f95744065a4bc688b1bc636f5d350
František Hrdina <hrdina@ics.muni.cz>
2018-08-17 11:27:18 +0200

    Seperate domain name addresses and ip addresses to different lists

--- mining-pools-collector.sh
+++ mining-pools-collector.sh
@@ -18,37 +18,61 @@ wget $CSV1 -O /tmp/csv1.txt
 cat /tmp/plain1.txt /tmp/plain2.txt > /tmp/plain.txt
 cat /tmp/csv1.txt > /tmp/csv.txt
 
-OUTPUT_FILE="pools.txt"
+OUTPUT_FILE_NAMES="pools-name.txt"
+OUTPUT_FILE_IPS="pools-IP.txt"
 
-POOLS=()
+POOLS_NAMES=()
+POOLS_IPS=()
 
 # Processing pools from plain text format
 while IFS='' read -r line || [[ -n "$line" ]]; do
-   if [[ ! " ${POOLS[@]} " =~ " ${line} " ]]; then
-      POOLS+=(${line})
+   if [[ "$line" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+      if [[ ! " ${POOLS_IPS[*]} " =~ " ${line} " ]]; then
+         POOLS_IPS+=("${line}")
+      fi 
+   else
+      if [[ ! " ${POOLS_NAMES[*]} " =~ " ${line} " ]]; then
+         POOLS_NAMES+=("${line}")
+      fi
+
    fi
+
 done  < "/tmp/plain.txt"
 rm /tmp/plain*
 
 # Processing pools from csv format
 while IFS='' read -r line || [[ -n "$line" ]]; do
    IFS=',' read -r -a array <<< "$line"
-   if [[ ! " ${POOLS[@]} " =~ " ${array[0]} " ]]; then
-      POOLS+=(${array[0]})
+   
+   if [[ "${array[0]}" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+      if [[ ! " ${POOLS_IPS[*]} " =~ " ${array[0]} " ]]; then
+         POOLS_IPS+=("${array[0]}")
+      fi 
+   else
+      if [[ ! " ${POOLS_NAMES[*]} " =~ " ${array[0]} " ]]; then
+         POOLS_NAMES+=("${array[0]}")
+      fi
+
    fi
 
 done < "/tmp/csv.txt"
 rm /tmp/csv*
 
 # Printing unique pools to file
-echo -n "" > $OUTPUT_FILE
+echo -n "" > $OUTPUT_FILE_NAMES
+echo -n "" > $OUTPUT_FILE_IPS
 
-for line in ${POOLS[@]}; do
-   echo ${line} >> $OUTPUT_FILE
+for line in "${POOLS_NAMES[@]}"; do
+   echo "${line}" >> $OUTPUT_FILE_NAMES
 done
 
-echo "Unique pools count:"
-wc -l $OUTPUT_FILE
+for line in "${POOLS_IPS[@]}"; do
+   echo "${line}" >> $OUTPUT_FILE_IPS
+done
 
+echo "Unique pools name count:"
+wc -l $OUTPUT_FILE_NAMES
 
+echo "Unique pools IPs count:"
+wc -l $OUTPUT_FILE_IPS
 
